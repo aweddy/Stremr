fb6587d5b2322241fb7685b9a7839e56ef6e95e0
Andrew Eddy <andrew.eddy@oracle.com>
2018-08-28 21:48:21 -0400

    static & api change not tied to /

--- webserver/index.js
+++ webserver/index.js
@@ -8,17 +8,19 @@ app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-// Swig will cache templates for you, but you can disable
-// that and use Express's caching instead, if you like:
 app.set('view cache', false);
-// To disable Swig's cache, do the following:
 swig.setDefaults({ cache: false });
-// NOTE: You should always cache templates in a production environment.
-// Don't leave both of these to `false` in production!
+
+app.use('/static', express.static('public'))
+
 app.get('/api/:category', function (req, res) {
     let cat = req.params.category;
-    var articles = fs.readFileSync(__dirname + '/JSON/'+ cat +'.json', 'utf8');
-    res.send(articles);
+    try{
+        var articles = fs.readFileSync(__dirname + '/JSON/'+ cat +'.json', 'utf8');
+        res.send(articles);
+    }catch(error){
+        res.send(error);
+    }
 });
 
 app.get('/', function (req, res) {
